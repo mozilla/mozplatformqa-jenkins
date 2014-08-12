ef9fa20340086625f3af24b302db5816fec05622
Syd Polk <spolk@mozilla.com>
2014-08-12 18:26:38 -0500

    New files for Jenkins.

---
+++ maintain_firefox_cache.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+platform=$1
+
+function repackage_mac_dmg {
+    if [ $platform = 'mac' ]; then
+        if [ firefox-latest-nightly.en-US.mac.dmg -nt firefox-latest-nightly.en-US.mac.tar.bz2 ]; then
+            hdiutil attach firefox-latest-nightly.en-US.mac.dmg
+            mkdir -p /tmp/releases/mac
+            rsync -avz --extended-attributes /Volumes/Nightly/FirefoxNightly.app /tmp/releases/mac/
+            cd /tmp/releases/mac
+            tar cvfj $wd/releases/firefox-latest-nightly.en-US.mac.tar.bz2 ./FirefoxNightly.app
+            cd $wd/releases
+            umount /Volumes/Nightly
+            rm -rf /tmp/releases/mac/FirefoxNightly.app
+        fi
+   fi
+}
+
+if [ $platform = "" ]; then
+    exit
+elif [ $platform = 'linux64' ]; then
+    web_platform='linux-x86_64'
+    archive_ext='tar.bz2'
+elif [ $platform = 'linux' ]; then
+    web_platform='linux-i686'
+    archive_ext='tar.bz2'
+elif [ $platform = 'mac' ]; then
+    web_platform='mac'
+    archive_ext='dmg'
+elif [ $platform = 'win' ]; then
+    web_platform='win'
+    archive_ext='zip'
+fi
+
+target="firefox-latest-nightly.en-US.$web_platform.$archive_ext"
+
+wd=`pwd`
+mkdir -p releases
+cd releases
+
+mozdownload --type=daily --platform=$platform
+
+if [ -e $target ]; then
+   if [ `find . -type f -name \*.$web_platform.$archive_ext -newer $target` ]; then
+	find . -type f -name \*.$web_platform.$archive_ext -not -newer $target -not -samefile $target -print -exec mv '{}' /tmp \;
+	find . -type f -name \*.$web_platform.$archive_ext -newer $target -print -exec ./mk_link.sh $target '{}' \;
+        repackage_mac_dmg
+   fi
+else
+    repackage_mac_dmg
+    ln -s *mozilla*central*$web_platform.$archive_ext $target
+fi
+
+cd $wd
+
+
---
+++ mk_link.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+rm -f $1
+ln -s $2 $1
+
