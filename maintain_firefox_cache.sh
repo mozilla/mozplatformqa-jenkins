654aa9816e2f136777e9928243bca6914da4527c
Syd Polk <spolk@mozilla.com>
2014-08-22 16:00:13 -0500

    Add support for "aurora" release.

--- maintain_firefox_cache.sh
+++ maintain_firefox_cache.sh
@@ -1,25 +1,47 @@
 #!/bin/bash
 
 platform=$1
+if [ "$2" = "" ]; then
+    release="nightly"
+else
+    release="$2"
+fi
+
+function usage {
+    "Usage: maintain_firefox_cache.sh <platform> |<release>|"
+    "<release> can be one of nightly, aurora, beta, release and esr."
+    exit 1
+}
 
 function repackage_mac_dmg {
     if [ $platform = 'mac' ] || [ $platform = 'mac64' ]; then
-        if [ ! -e firefox-latest-nightly.en-US.$platform.tar.bz2 ] || [ firefox-latest-nightly.en-US.$platform.dmg -nt firefox-latest-nightly.en-US.$platform.tar.bz2 ]; then
-            hdiutil attach firefox-latest-nightly.en-US.$platform.dmg
+        if [ ! -e firefox-latest-$release.en-US.$platform.tar.bz2 ] || [ firefox-latest-$release.en-US.$platform.dmg -nt firefox-latest-$release.en-US.$platform.tar.bz2 ]; then
+            hdiutil attach firefox-latest-$release.en-US.$platform.dmg
             mkdir -p /tmp/releases/$platform
-            rsync -avz --extended-attributes /Volumes/Nightly/FirefoxNightly.app /tmp/releases/$platform/
-            rm -f firefox-latest-nightly.en-US.$platform.tar.bz2
+            if [ "$release" = "nightly" ]; then
+                volname="Nightly"
+                appname="FirefoxNightly"
+            elif [ "$release" = "aurora" ]; then
+                volname="Aurora"
+                appname="FirefoxAurora"
+            else
+                volname="Firefox"
+                appname="Firefox"
+            fi
+
+            rsync -avz --extended-attributes /Volumes/$volname/$appname.app /tmp/releases/$platform/
+            rm -f firefox-latest-$release.en-US.$platform.tar.bz2
             pushd /tmp/releases/$platform
-            tar cvfj $wd/releases/firefox-latest-nightly.en-US.$platform.tar.bz2 ./FirefoxNightly.app
+            tar cvfj $wd/releases/firefox-latest-$release.en-US.$platform.tar.bz2 ./$appname.app
             popd
-            umount /Volumes/Nightly
-            rm -rf /tmp/releases/$platform/FirefoxNightly.app
+            umount /Volumes/$volname
+            rm -rf /tmp/releases/$platform/$appname.app
         fi
    fi
 }
 
 if [ "$platform" = "" ]; then
-    exit
+    usage
 elif [ $platform = 'linux64' ]; then
     web_platform='linux-x86_64'
     archive_ext='tar.bz2'
@@ -40,22 +62,34 @@ elif [ $platform = 'win64' ]; then
     archive_ext='zip'
 fi
 
-target="firefox-latest-nightly.en-US.$web_platform.$archive_ext"
-
 wd=`pwd`
 mkdir -p releases
 cd releases
 
-mozdownload --type=daily --platform="$platform" --extension="$archive_ext"
+target="firefox-latest-$release.en-US.$web_platform.$archive_ext"
 
-if [ -e $target ]; then
-    if [ `find . -type f -name \*.$web_platform.$archive_ext -newer $target` ]; then
-	find . -type f -name \*.$web_platform.$archive_ext -not -newer $target -not -samefile $target -print -exec mv '{}' /tmp \;
-	find . -type f -name \*.$web_platform.$archive_ext -newer $target -print -exec ../copy_latest.sh '{}' $target \;
+if [ "$release" = "nightly" ] || [ "$release" = "aurora" ] ; then
+    if [ "$release" = "aurora" ]; then
+        mozdownload --type=daily --platform="$platform" --extension="$archive_ext" --branch=mozilla-aurora
+    else
+        mozdownload --type=daily --platform="$platform" --extension="$archive_ext"
     fi
-else
-    ../copy_latest.sh *mozilla*central*$web_platform.$archive_ext $target
+
+    if [ -e $target ]; then
+        if [ `find . -type f -name \*.$web_platform.$archive_ext -newer $target` ]; then
+	    find . -type f -name \*.$web_platform.$archive_ext -not -newer $target -not -samefile $target -print -exec mv '{}' /tmp \;
+	    find . -type f -name \*.$web_platform.$archive_ext -newer $target -print -exec ../copy_latest.sh '{}' $target \;
+        fi
+    else
+        if [ "$release" = "nightly" ]; then
+            tag="central"
+        else
+            tag="$release"
+        fi
+        ../copy_latest.sh *mozilla*$tag*$web_platform.$archive_ext $target
+    fi
 fi
+
 repackage_mac_dmg
 
 cd $wd
